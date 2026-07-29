32ded197cbd0ff6b51a76574dfcc293885b2dc05
Saikat Talukder <saikat60mu@gmail.com>
2026-07-29 15:05:19 +0600

    Add arithmetic and assignment operators example

---
+++ js/2. Arithmetic and Assignment Operators.js
@@ -0,0 +1,10 @@
+let a = 10, b = 3;
+console.log(a + b); // 13
+console.log(a - b); // 7
+console.log(a * b); // 30
+console.log(a / b); // 3.3333333333333335
+console.log(a % b); // 1 (remainder / modulus)
+console.log(a ** b); // 1000 (10 to the power 3)
+let x = 5;
+x += 2; // same as x = x + 2 -> 7
+x++; // increment by 1 -> 8
