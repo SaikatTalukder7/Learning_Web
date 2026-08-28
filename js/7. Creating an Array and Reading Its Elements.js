da38187f7d351bd1ea97b485ddcc2bd8a5a972b9
Saikat Talukder <saikat60mu@gmail.com>
2026-08-28 08:11:11 +0600

    Add array creation and element access examples

---
+++ js/7. Creating an Array and Reading Its Elements.js
@@ -0,0 +1,13 @@
+const marks = [78, 92, 65, 88];
+
+console.log(marks[0]);
+console.log(marks[2]);
+console.log(marks.length);
+
+marks[1] = 95;
+
+console.log(marks);
+
+console.log(marks[marks.length - 1]);
+console.log(marks.at(-1));
+console.log(marks.at(-2));
